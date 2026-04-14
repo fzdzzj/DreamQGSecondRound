282819d4c386684747111dc8817becc53447d5d0
凤媚珍 <2574862723@qq.com>
2026-04-14 13:52:48 +0800

    fix:改密码和登录用验证码时先判断是否存在 feat:获取验证码的接口

---
+++ backend/lostfound/pojo/src/main/java/com/qg/pojo/dto/EmailSendCodeDTO.java
@@ -0,0 +1,21 @@
+package com.qg.pojo.dto;
+
+import com.qg.common.constant.RegexConstant;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Schema(description = "邮箱验证码DTO")
+@Data
+public class EmailSendCodeDTO implements Serializable {
+    @Schema(description = "邮箱")
+    @Pattern(regexp= RegexConstant.EMAIL, message = "邮箱格式错误")
+    @NotBlank(message = "邮箱不能为空")
+    private String email;
+    @Schema(description = "类型")
+    @NotBlank(message = "类型不能为空,1.REGISTER / 2.LOGIN / 3.RESET_PASSWORD")
+    private String type;
+}
