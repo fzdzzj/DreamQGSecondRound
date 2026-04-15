edfc0eb2601c65fb6053527e25bd1ada3342d18b
凤媚珍 <2574862723@qq.com>
2026-04-15 17:17:21 +0800

    feat:增强Pageresult

--- backend/lostfound/common/src/main/java/com/qg/common/result/PageResult.java
+++ backend/lostfound/common/src/main/java/com/qg/common/result/PageResult.java
@@ -1,36 +1,26 @@
 package com.qg.common.result;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.List;
-
 /**
- * 分页结果类
+ * 分页结果
+ *
+ * @param <T>
  */
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class PageResult<T> implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    /**
-     * 数据列表
-     */
     private List<T> list;
-    /**
-     * 总记录数
-     */
     private Long total;
-    /**
-     * 当前页码
-     */
     private Integer pageNum;
-    /**
-     * 每页记录数
-     */
     private Integer pageSize;
 
+    public PageResult(List<T> list, Long total, Integer pageNum, Integer pageSize) {
+        this.list = list;
+        this.total = total;
+        this.pageNum = pageNum;
+        this.pageSize = pageSize;
+    }
 }
