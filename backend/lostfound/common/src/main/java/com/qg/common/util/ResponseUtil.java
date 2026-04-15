a864a7aff8cc93b6c85747cf1edb6a0a1e992283
凤媚珍 <2574862723@qq.com>
2026-04-15 17:21:10 +0800

    feat:统一响应输出工具

---
+++ backend/lostfound/common/src/main/java/com/qg/common/util/ResponseUtil.java
@@ -0,0 +1,15 @@
+package com.qg.common.util;
+
+import com.alibaba.fastjson2.JSON;
+import com.qg.common.result.Result;
+
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ResponseUtil {
+
+    public static void write(HttpServletResponse response, Result<?> result) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(JSON.toJSONString(result));
+    }
+}
